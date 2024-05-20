e3fefd9baedec2ab9919b1a5b5c5db0e154d946f
cristinamartinez1394 <crismartinez1394@gmail.com>
2024-05-20 11:31:53 -0500

    Creacion de archivos para product details page

---
+++ cypress/e2e/pages/product-details/product-details.elements.js
@@ -0,0 +1,7 @@
+import { ProductDetailsElements } from "./product-details.methods";
+
+export class ProdcutsDetailsMethods{
+    static clickOnAddToCartbutton(){
+        ProductDetailsElements.buttons.addToCart.click();
+    }
+}
