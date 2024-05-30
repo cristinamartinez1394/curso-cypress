1aaa758c9df6d00f740951337d20b14e999edfcd
cristinamartinez1394 <crismartinez1394@gmail.com>
2024-05-30 11:23:41 -0500

    Crear usuario aleatorio

--- cypress/e2e/tests/registro-y-autenticacion.cy.js
+++ cypress/e2e/tests/registro-y-autenticacion.cy.js
@@ -2,6 +2,8 @@ import { CommonPageData } from "../pages/common-page/common-page.data";
 import { CommonPageMethods } from "../pages/common-page/common-page.methods";
 import { SignupMethods } from "../pages/signup/signup.methods";
 import { Logger } from "../util/logger";
+const user = CommonPageMethods.generateRandomString();
+const password = CommonPageMethods.generateRandomString(7);
 
 describe(CommonPageData.testSuites.registroYAutenticacion,()=>{
     it("Registro de usuario válido",()=>{
@@ -15,13 +17,12 @@ describe(CommonPageData.testSuites.registroYAutenticacion,()=>{
 
         Logger.stepNumber(3)
         Logger.step('Completar los campos obligatorios con información válida')
-        SignupMethods.insertUsername('testinguser1')
-        SignupMethods.insertPassword('testinguser1')
+        SignupMethods.insertUsername(user)
+        SignupMethods.insertPassword(password)
 
         Logger.stepNumber(4)
         Logger.step('Hacer clic en "Sign up" para registrar el usuario')
         SignupMethods.clickOnSignupButton();
-        cy.wait(5000)
         Logger.verification('Verificar que se muestre el mensaje "Sign up successful."')
         SignupMethods.verifySignupSuccessfulMessageIsDisplayed();
      });
