e3687bfb9134d572365bbcaa1248bf58a3e8f3bf
cristinamartinez1394 <crismartinez1394@gmail.com>
2024-05-31 10:55:05 -0500

    Caso de prueba inicio de sesion inválido

--- cypress/e2e/tests/atutenticacion.cy.js
+++ cypress/e2e/tests/atutenticacion.cy.js
@@ -25,4 +25,25 @@ describe(CommonPageData.testSuites.autenticacion, ()=>{
         Logger.verification('Verificar que se redirige al usuario a la página de inicio')
         CommonPageMethods.verifySignedUser(LoginData.validCredentials.username)
     })
+
+    it('Inicio de sesion inválido', ()=>{
+        Logger.stepNumber(1)
+        Logger.step('Navegar a la página de inicio')
+        CommonPageMethods.navigateToDemoblaze();
+
+        Logger.stepNumber(2)
+        Logger.step('Hacer clic en "Log in" en la barra de navegación')
+        CommonPageMethods.clicknLoginOption();
+
+        Logger.stepNumber(3)
+        Logger.step('Ingresar un nombre de usuario y contraseñas inválidos')
+        LoginMethods.insertUsername(LoginData.validCredentials.username)
+        LoginMethods.insertPassword('contraseñainvalida')
+
+        Logger.stepNumber(4)
+        Logger.step('Hacer clic en "Log in" para inciar sesion')
+        LoginMethods.clickOnloginButton();
+        Logger.verification('Verificar que se muestra un mensaje de error indicando que el inicio de sesion ha fallado')
+        LoginMethods.verifiWrongPasswordMessage();
+    })
 })
